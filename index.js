b7ba179c4721bb0144693a389547d1aa475c4cce
MostafaMaged00 <185123397+MostafaMaged00@users.noreply.github.com>
2024-11-07 14:10:55 -0500

    First loop

---
+++ index.js
@@ -0,0 +1,8 @@
+// Fizz Buzz
+// .............................................................
+// Loop through all numbers from 1 to 100.
+for(let x = 0; x<=10;x++){
+    console.log(x)
+    if(x%3==0)
+        console.log(`The number ${x} is divisible by 3\nFizz`)
+}
