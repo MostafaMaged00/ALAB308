2f932e833c5fb5011062c86872f14408cfd004c8
MostafaMaged00 <185123397+MostafaMaged00@users.noreply.github.com>
2024-11-07 14:27:33 -0500

    Finish Part 1

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 // Fizz Buzz
 // .............................................................
 // Loop through all numbers from 1 to 100.
-for(let x = 0; x<=10;x++){
-    console.log(x)
-    if(x%3==0)
-        console.log(`The number ${x} is divisible by 3\nFizz`)
+for(let x = 1; x<=100;x++){
+
+    if (x%3==0 && x%5==0){
+        console.log(`Fizz - Bizz The number ${x} is divisible by 3 & 5 `)
+ 
+    }   
+    else if(x%3==0){
+        console.log(`Fizz - The number ${x} is divisible by 3`)
+    }
+    else if (x%5==0 ){
+        console.log(`Bizz - The number ${x} is divisible by 5`)
+ 
+    } 
+    else{
+        console.log(x)
+    }
+
 }
+
+// .............................................................
