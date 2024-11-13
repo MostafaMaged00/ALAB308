352146eb098233469f8ebfd8e9339bf702254bbb
MostafaMaged00 <185123397+MostafaMaged00@users.noreply.github.com>
2024-11-13 11:36:00 -0500

    start second part of assign.

--- index.js
+++ index.js
@@ -1,23 +1,43 @@
 // Fizz Buzz
 // .............................................................
 // Loop through all numbers from 1 to 100.
-for(let x = 1; x<=100;x++){
+// for(let x = 1; x<=100;x++){
 
-    if (x%3==0 && x%5==0){
-        console.log(`Fizz - Bizz The number ${x} is divisible by 3 & 5 `)
+//     if (x%3==0 && x%5==0){
+//         console.log(`Fizz - Buzz The number ${x} is divisible by 3 & 5 `)
  
-    }   
-    else if(x%3==0){
-        console.log(`Fizz - The number ${x} is divisible by 3`)
-    }
-    else if (x%5==0 ){
-        console.log(`Bizz - The number ${x} is divisible by 5`)
+//     }   
+//     else if(x%3==0){
+//         console.log(`Fizz - The number ${x} is divisible by 3`)
+//     }
+//     else if (x%5==0 ){
+//         console.log(`Buzz - The number ${x} is divisible by 5`)
  
-    } 
-    else{
-        console.log(x)
-    }
+//     } 
+//     else{
+//         console.log(x)
+//     }
 
-}
+// }
 
 // .............................................................
+// Part 2: Prime Time
+
+
+for (let n = 2; n < 30; n++) {
+
+
+   if(n%2!=0 && n%3 != 0 && n%5 != 0){
+  console.log(n + ' is prime numer')
+  
+   }else{
+
+
+   }
+
+
+  }
+
+
+//   ****************************************************************************
+// Part TWO
