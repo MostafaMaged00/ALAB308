36f16a701550290930fc13a933f0074251d14fc7
MostafaMaged00 <185123397+MostafaMaged00@users.noreply.github.com>
2024-11-14 07:56:35 -0500

    still trying

--- index.js
+++ index.js
@@ -24,20 +24,91 @@
 // Part 2: Prime Time
 
 
-for (let n = 2; n < 30; n++) {
+// for (let n = 2; n < 30; n++) {
 
 
-   if(n%2!=0 && n%3 != 0 && n%5 != 0){
-  console.log(n + ' is prime numer')
+//    if(n%2!=0 && n%3 != 0 && n%5 != 0){
+//   console.log(n + ' is prime numer')
   
-   }else{
+//    }else{
 
 
-   }
+//    }
 
 
-  }
+//   }
 
 
 //   ****************************************************************************
-// Part TWO
+
+const csv = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
+const colums = 4
+const rows = 4
+let cell0,cell1,cell2,cell3
+
+//const rows = csv.split('\n')
+//  console.log(rows);
+
+
+let line =rows[0].split('\,')
+
+
+for (let i = 0; i < 4; i++) {
+  cell0 = (line[i].split('\,'))
+
+}
+
+ console.log(cell0)
+
+// for (let i = 0; i < 4; i++) {
+//       let cell = line[i].split('\,')
+//       console.log(cell)
+//     }
+// console.log(rows[1].split('\,'))
+// console.log(rows[2].split('\,'))
+// console.log(rows[3].split('\,'))
+// console.log(rows[4].split('\,'))
+
+// for (let i = 0; i < 4; i++) {
+//    console.log(rows[i].split('\,'))
+//  }
+
+// for (let i = 0; i < 4; i++) {
+//    console.log(csv.split('\n'));
+//  }
+
+//   ****************************************************************************
+// Part 2: Expanding Functionality
+// let col = 4;
+
+
+
+// function computeArea (width , height){
+//    return "The rectangler area is : "+ width * height + " SQF";
+
+// }
+
+// computeArea(2,4)
+
+// ***************************
+// function planetHasWater(planet){
+
+//    let plantlower = planet.toLowerCase();
+//    if(plantlower == "earth" || plantlower == "mars"){
+//       return true + " !!The planet has water";
+//    }
+//    else{
+//       return false + " :( The planet has water";;
+//    }
+
+// }
+// const planetHasWater = function(planet){
+//    let plantlower = planet.toLowerCase();
+//    if(plantlower == "earth" || plantlower == "mars"){
+//       return true + " !!The planet has water";
+//    }
+//    else{
+//       return false + " :( The planet has water";;
+//    }
+// }
+// planetHasWater('eArth');
